aed097814bb1c257db709863730e1ffc03efdb3b
Olamilekan02J <140506368+Olamilekan02J@users.noreply.github.com>
2023-11-22 23:17:04 +0100

    Update chatBotSpeechReg.py

--- chatBotSpeechReg.py
+++ chatBotSpeechReg.py
@@ -13,9 +13,9 @@ warnings.filterwarnings('ignore')
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
 # Download required NLTK data
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
 
 
 data = pd.read_csv('Mental_Health_FAQ (2).csv')
